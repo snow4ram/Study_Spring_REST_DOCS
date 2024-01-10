f3d40a0f9f0f73a0bc26b8d898998ddd9ece53f6
황중석 <hjs7949@naver.com>
2024-01-10 17:52:52 +0900

    no message

--- src/main/java/spring_rest_docs/rest_docs/repository/JpaMemberRpository.java
+++ src/main/java/spring_rest_docs/rest_docs/repository/JpaMemberRpository.java
@@ -1,7 +1,7 @@
 package spring_rest_docs.rest_docs.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import spring_rest_docs.rest_docs.member.MemberEntity;
+import spring_rest_docs.rest_docs.entity.MemberEntity;
 
 public interface JpaMemberRpository extends JpaRepository<MemberEntity, Long> {
 }
