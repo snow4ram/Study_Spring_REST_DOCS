2148376a046543eec8e1a99e86749ca5dec57bba
황중석 <hjs7949@naver.com>
2024-01-11 17:53:29 +0900

    Member Class 에서 Member DTO Class 변경

--- src/main/java/spring_rest_docs/rest_docs/dto/MemberDTOMapper.java
+++ src/main/java/spring_rest_docs/rest_docs/dto/MemberDTOMapper.java
@@ -1,15 +1,15 @@
 package spring_rest_docs.rest_docs.dto;
 
 import org.springframework.stereotype.Service;
-import spring_rest_docs.rest_docs.entity.MemberEntity;
+import spring_rest_docs.rest_docs.entity.Member;
 
 import java.util.function.Function;
 
 
 @Service
-public class MemberDTOMapper implements Function<MemberEntity , MemberDTO> {
+public class MemberDTOMapper implements Function<Member, MemberDTO> {
     @Override
-    public MemberDTO apply(MemberEntity memberEntity) {
+    public MemberDTO apply(Member memberEntity) {
         return new MemberDTO(
                 memberEntity.getId(),
                 memberEntity.getEmail(),
