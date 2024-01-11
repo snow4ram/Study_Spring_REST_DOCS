bfa7bd801bf70ba67c2659a0e2923e81ec5c70f5
황중석 <hjs7949@naver.com>
2024-01-11 17:52:13 +0900

    회원 초기 설정

--- src/main/java/spring_rest_docs/rest_docs/AppConfig.java
+++ src/main/java/spring_rest_docs/rest_docs/AppConfig.java
@@ -3,20 +3,20 @@ package spring_rest_docs.rest_docs;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import spring_rest_docs.rest_docs.entity.MemberEntity;
-import spring_rest_docs.rest_docs.repository.JpaMemberRpository;
+import spring_rest_docs.rest_docs.entity.Member;
+import spring_rest_docs.rest_docs.repository.JpaMemberRepository;
 
 @Configuration
 @RequiredArgsConstructor
 public class AppConfig {
 
-    private final JpaMemberRpository repository;
+    private final JpaMemberRepository repository;
 
     @PostConstruct
     public void init() {
 
         for (int i = 0; i < 10; i++) {
-            repository.save(new MemberEntity(Name.generateRandomEmail(), Name.randomKoreanName()));
+            repository.save(new Member(Name.generateRandomEmail(), Name.randomKoreanName()));
         }
     }
 }
